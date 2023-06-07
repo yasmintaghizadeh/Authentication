2ac47a2e2c2d81a42798d66708597b8343e3df97
yasmin <yasmin.taghizadeh@gmail.com>
2023-06-07 17:21:14 +0330

    security config application config

--- src/main/java/com/test/auth/config/JwtAuthenticationFilter.java
+++ src/main/java/com/test/auth/config/JwtAuthenticationFilter.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,6 +19,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authenticationHeader = request.getHeader("Authorization");
@@ -25,5 +31,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwtToken=authenticationHeader.substring(7);
         userName=jwtService.extractUsername(jwtToken);
+        if (userName!=null&& SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails=this.userDetailsService.loadUserByUsername(userName);
+            if(jwtService.isTokenValid(jwtToken,userDetails)){
+                UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+           authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+           SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
--- src/main/java/com/test/auth/repository/UserRepository.java
+++ src/main/java/com/test/auth/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity,String> {
 
     Optional<UserEntity> findByPhoneNumber(Long phoneNumber);
+    Optional<UserEntity> findByUsername(String userName);
 }
